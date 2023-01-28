4867a41167c83e0a2f797411920e28a6dae0cc21
Duy Hoang <duyhoang123@hotmail.co.uk>
2023-01-28 17:35:24 +0100

    Adding email preferences and DBIO

---
+++ src/main/scala/EmailPreferences.scala
@@ -0,0 +1,26 @@
+import scala.concurrent.ExecutionContext
+
+object EmailPreferences {
+
+  trait Emails[F[_]]{
+    def save(email: Email): F[Either[EmailAlreadyExists.type, Email]]
+    def known(email: Email): F[Boolean]
+    def findEmail(email: Email): F[Option[Email]]
+  }
+
+  class UserRepositoy(emailRepository: EmailRepository)(
+                     implicit ec: ExecutionContext) extends Users[DBIO] {
+    override def createUser(primaryEmail: Email, userProfile: UserProfile) = {
+      val row = DbUser.from(primaryEmail, userProfile)
+      (UsersTable += row).map(PersistedUser(_, row))
+    }
+    override def identifyUser(email: Email) = identifyQuery(email).result.flatMap{
+      case Seq()          => DBIO.successful(None)
+      case Seq(singleRow) => DBIO.successful(Some(singleRow))
+      case _              => DBIO.failed(
+        new IllegalStateException(s"More than one user uses email: $email")
+      )
+    }
+  }
+
+}
